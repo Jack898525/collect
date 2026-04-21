be035563ec0d19f936312b29feac7b437fa6e6c8
Jack <1279301891@qq.com>
2026-04-21 16:45:39 +0800

    chore: temporarily disable admin route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/pages/HomePage.vue'
-import AdminView from '@/pages/AdminView.vue'
+// import AdminView from '@/pages/AdminView.vue'
 
 // 定义路由配置
 const routes = [
@@ -8,12 +8,12 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomePage,
-  },
-  {
-    path: '/admin',
-    name: 'admin',
-    component: AdminView,
-  },
+  }
+  // {
+  //   path: '/admin',
+  //   name: 'admin',
+  //   component: AdminView,
+  // },
 ]
 
 // 创建路由实例
